54102d8aa6912fd4849b461882a614f806bef2c2
Alexander Ein <435658+anein@users.noreply.github.com>
2022-05-31 02:57:50 +0300

    feat: open new tab when click on action button

---
+++ background/background.js
@@ -0,0 +1,6 @@
+// if user clicks on the action button, open Yandex in a new tab.
+browser.browserAction.onClicked.addListener(function () {
+  browser.tabs.create({
+    url: browser.i18n.getMessage("browser_action_url")
+  });
+});
