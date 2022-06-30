08ebcd6189e5f4e785fbd5591deaff3c9d77b8b5
Alexander Ein <435658+anein@users.noreply.github.com>
2022-06-30 21:25:36 +0300

    fix: add use strict mode

--- src/background/background.js
+++ src/background/background.js
@@ -1,6 +1,8 @@
+'use strict';
+
 // if user clicks on the action button, open Yandex in a new tab.
 browser.browserAction.onClicked.addListener(function () {
   browser.tabs.create({
-    url: browser.i18n.getMessage("browser_action_url")
+    url: browser.i18n.getMessage('browser_action_url')
   });
 });
