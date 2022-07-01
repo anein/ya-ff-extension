b68069cead24f38bbacf2877a9f34e9fdfdd2427
Alexander Ein <435658+anein@users.noreply.github.com>
2022-07-01 03:09:40 +0300

    feat: store default options in db on ext installed

--- src/background/background.js
+++ src/background/background.js
@@ -1,8 +1,13 @@
 'use strict';
 
-// if user clicks on the action button, open Yandex in a new tab.
-browser.browserAction.onClicked.addListener(function () {
-  browser.tabs.create({
-    url: browser.i18n.getMessage('browser_action_url')
-  });
+// Write default options
+browser.runtime.onInstalled.addListener(()=>{
+  const options = {
+    ya_search: false,
+    ya_fonts: false,
+    ya_font_size: false,
+    ya_side_panel: false
+  };
+
+  browser.storage.local.set({ options });
 });
