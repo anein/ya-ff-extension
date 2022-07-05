28c50c2b7a673f59945702fc87321a4d865a3aea
Alexander Ein <435658+anein@users.noreply.github.com>
2022-07-05 19:35:32 +0300

    feat: insert styles programmerly

--- src/background/background.js
+++ src/background/background.js
@@ -1,13 +1,93 @@
-'use strict';
+"use strict";
 
-// Write default options
-browser.runtime.onInstalled.addListener(()=>{
-  const options = {
-    ya_search: false,
-    ya_fonts: false,
-    ya_font_size: false,
-    ya_side_panel: false
-  };
+const DEFAULT_OPTIONS = {
+  ya_search: false,
+  ya_fonts: false,
+  ya_font_size: false,
+  ya_side_panel: false,
+};
 
-  browser.storage.local.set({ options });
+// let scriptRegistered = null;
+
+// Write default options on installed.
+browser.runtime.onInstalled.addListener(() => {
+  browser.storage.local.set({ DEFAULT_OPTIONS });
 });
+
+browser.runtime.onStartup.addListener(() => {
+  console.log("Extension has started.");
+  registerScript(null);
+});
+
+// listen message from the popup script.
+browser.runtime.onMessage.addListener((message) => {
+  if (message.type === "updateOptions") {
+    registerScript(message.options);
+  }
+});
+
+async function registerScript(message) {
+  let options = message;
+
+  // if (scriptRegistered) {
+  //   scriptRegistered.unregister();
+  // }
+
+  if (options === null) {
+    // retrieve options from the storage.
+    let data = await browser.storage.local.get("options");
+    options =
+      Object.entries(data).length === 0 ? DEFAULT_OPTIONS : data.options;
+  }
+
+  let registeredScripts = await browser.scripting.getRegisteredContentScripts();
+  let registeredScriptsIds = registeredScripts.map((script) => script.id);
+  let optionsInactiveIds = Object.keys(options).filter(
+    (element) => options[element] === false
+  );
+  let optionsActiveIds = Object.keys(options).filter(
+    (element) => options[element] === true
+  );
+
+  // get scripts for unload.
+  let intersectionActiveInactive = registeredScriptsIds.filter((value) =>
+    optionsInactiveIds.includes(value)
+  );
+
+  // remove disabled scripts from a list of registered scripts
+  registeredScriptsIds = registeredScriptsIds.filter(
+    (value) => intersectionActiveInactive.includes(value) === false
+  );
+
+  // keep only new scripts for adding
+  let optionsActiveList = optionsActiveIds.filter(
+    (value) => registeredScriptsIds.includes(value) === false
+  );
+
+  if (intersectionActiveInactive.length > 0) {
+    // remove inactive scripts if ones are  in the list.
+    await browser.scripting.unregisterContentScripts({
+      ids: intersectionActiveInactive,
+    });
+  }
+
+  if (optionsActiveList.length === 0) {
+    return "Nothing to add";
+  }
+
+  optionsActiveList = optionsActiveList.map((value) => {
+    return {
+      id: value,
+      matches: ["*://yandex.ru/search/*"],
+      css: [`content/${value}.css`],
+      persistAcrossSessions: false,
+    };
+  });
+
+  try {
+    await browser.scripting.registerContentScripts(optionsActiveList);
+  } catch (error) {
+    // console.error(error);
+  }
+  return true;
+}
--- src/popup/options.js
+++ src/popup/options.js
@@ -1,14 +1,14 @@
-'use strict';
+"use strict";
 
 // key name for storing and retrieving data from the local storage. Basically, for retrieving.
-const OPTIONS_STORAGE_KEY = 'options';
+const OPTIONS_STORAGE_KEY = "options";
 
 // default option names and values
 const DEFAULT_OPTIONS = {
   ya_search: false,
   ya_fonts: false,
   ya_font_size: false,
-  ya_side_panel: false
+  ya_side_panel: false,
 };
 
 /**
@@ -21,16 +21,17 @@ function onError(error) {
 }
 
 function onDataLoaded(options) {
-  const container = document.querySelector('.container');
+  const container = document.querySelector(".container");
 
   // Stores all elements of available options.
-  let optionsElements = '';
+  let optionsElements = "";
 
   // loop over options and create elements for the popup menu
   Object.entries(options).forEach((value, index) => {
     const [name, state] = [...value];
 
-    let stateElement = (index === 0) ? createCheckmark(state) : createCheckbox(name, state);
+    let stateElement =
+      index === 0 ? createCheckmark(state) : createCheckbox(name, state);
     optionsElements += createOptionElement(name, state, stateElement);
   });
 
@@ -48,11 +49,11 @@ function onDataLoaded(options) {
  */
 function createOptionElement(name, state, stateElement) {
   //
-  const title = browser.i18n.getMessage(name + '_title');
-  const description = browser.i18n.getMessage(name + '_description');
+  const title = browser.i18n.getMessage(name + "_title");
+  const description = browser.i18n.getMessage(name + "_description");
 
   return `
-  <section class="option-container ${state ? 'active' : ''}">
+  <section class="option-container ${state ? "active" : ""}">
     <div class="option-item">
       <div>
           <div class="title">${title}</div>
@@ -74,7 +75,7 @@ function createOptionElement(name, state, stateElement) {
  *
  */
 function createCheckmark(state) {
-  return (state) ? '<i class="checkmark"></i>' : '<i class="checkmark-sad"></i>';
+  return state ? '<i class="checkmark"></i>' : '<i class="checkmark-sad"></i>';
 }
 
 /**
@@ -88,7 +89,7 @@ function createCheckmark(state) {
 function createCheckbox(name, checked) {
   return `
      <label class="switch">
-        <input type="checkbox" ${(checked) ? 'checked' : ''} name="${name}">
+        <input type="checkbox" ${checked ? "checked" : ""} name="${name}">
         <span class="slider"></span>
     </label>`;
 }
@@ -97,12 +98,12 @@ function createCheckbox(name, checked) {
  * Sets input listeners on checkboxes, and save it to the storage on a state change.
  */
 function setInputLinters() {
-  const container = document.querySelector('.container');
+  const container = document.querySelector(".container");
 
   const inputs = container.querySelectorAll("input[type='checkbox']");
 
   inputs.forEach((element) => {
-    element.addEventListener('change', (event) => {
+    element.addEventListener("change", (event) => {
       // console.log(event.target.checked, event.target.name);
       saveOptions(event.target.name, event.target.checked);
     });
@@ -117,30 +118,36 @@ function setInputLinters() {
  */
 function saveOptions(name, value) {
   browser.storage.local.get(OPTIONS_STORAGE_KEY).then((data) => {
-    const options = Object.entries(data).length === 0 ? DEFAULT_OPTIONS : data.options;
+    const options =
+      Object.entries(data).length === 0 ? DEFAULT_OPTIONS : data.options;
     options[name] = value;
-    browser.storage.local.set({ options });
+
+    // send message to the background script to inject or remove content script
+    browser.storage.local.set({ options }).then(() => {
+      browser.runtime.sendMessage({
+        type: "updateOptions",
+        options,
+      });
+    });
   });
 }
 
 // load settings from the storage
 browser.storage.local.get(OPTIONS_STORAGE_KEY).then((data) => {
   // checking existence of options in the local storage
-  const options = Object.entries(data).length === 0 ? DEFAULT_OPTIONS : data.options;
+  const options =
+    Object.entries(data).length === 0 ? DEFAULT_OPTIONS : data.options;
 
   // getting search engine name
-  const searchEngineName = browser.runtime.getManifest()
-    .chrome_settings_overrides
-    .search_provider
-    .name;
+  const searchEngineName =
+    browser.runtime.getManifest().chrome_settings_overrides.search_provider
+      .name;
 
   // checking if the engine sets as default, then draw popup.
   browser.search.get().then((results) => {
-    let isDefaultEngine = !!(results.find((elem) => {
-      return (elem.name === searchEngineName && elem.isDefault);
-    }));
-
-    options.ya_search = isDefaultEngine;
+    options.ya_search = !!results.find((elem) => {
+      return elem.name === searchEngineName && elem.isDefault;
+    });
 
     onDataLoaded(options);
   });
