e129050b0563327785a976963dd59e2a81c248d0
Alexander Ein <435658+anein@users.noreply.github.com>
2022-08-16 15:39:12 +0300

    test: add tests for converting options

--- tests/background.test.js
+++ tests/background.test.js
@@ -4,33 +4,35 @@ import vm from "node:vm";
 
 const path = "./src/background/background.js";
 
-const code = fs.readFileSync(path);
+const code = fs.readFileSync(path).toString();
 vm.runInThisContext(code);
 
-describe("Background", function () {
+describe("Background Tests", function () {
+  let mockOptions = [];
+
+  beforeEach(() => {
+    mockOptions = Array.from(getDefaultOptions());
+  });
+
   describe(".isNameInOptions()", function () {
     it("Should exist Function", function () {
       expect(isNameInOptions).to.not.undefined;
     });
 
-    it("Should exist .DEFAULT_OPTIONS", function () {
-      expect(DEFAULT_OPTIONS).to.not.undefined;
-    });
-
     it("Should find 'ya_search' in the options", function () {
-      expect(isNameInOptions(DEFAULT_OPTIONS, "ya_search")).to.be.true;
+      expect(isNameInOptions(mockOptions, "ya_search")).to.be.true;
     });
 
     it("Should find nested name 'ya_fonts' in the options", function () {
-      expect(isNameInOptions(DEFAULT_OPTIONS, "ya_fonts")).to.be.true;
+      expect(isNameInOptions(mockOptions, "ya_fonts")).to.be.true;
     });
 
     it("Should not find the empty name in the options", function () {
-      expect(isNameInOptions(DEFAULT_OPTIONS, null)).to.be.false;
+      expect(isNameInOptions(mockOptions, null)).to.be.false;
     });
 
     it("Should not find the unknown name in the options", function () {
-      expect(isNameInOptions(DEFAULT_OPTIONS, "unknown_name")).to.be.false;
+      expect(isNameInOptions(mockOptions, "unknown_name")).to.be.false;
     });
   });
 
@@ -39,47 +41,35 @@ describe("Background", function () {
       expect(changeOptionState).to.be.not.undefined;
     });
 
-    it("Should exist .DEFAULT_OPTIONS", function () {
-      expect(DEFAULT_OPTIONS).to.not.undefined;
-    });
-
     it("Should change 'ya_search' state to false", function () {
-      const options = changeOptionState(DEFAULT_OPTIONS, "ya_search", false);
+      const options = changeOptionState(mockOptions, "ya_search", false);
       expect(options[0].state).to.be.false;
     });
 
     it("Should change 'ya_search' state to true", function () {
-      const options = changeOptionState(DEFAULT_OPTIONS, "ya_search", true);
+      const options = changeOptionState(mockOptions, "ya_search", true);
       expect(options[0].state).to.be.true;
     });
 
     it("Should change 'ya_group_panels' state to false", function () {
-      const options = changeOptionState(
-        DEFAULT_OPTIONS,
-        "ya_group_panels",
-        false
-      );
+      const options = changeOptionState(mockOptions, "ya_group_panels", false);
       expect(options[3].state).to.be.false;
     });
 
     it("Should change 'ya_group_panels' state to true", function () {
-      const options = changeOptionState(
-        DEFAULT_OPTIONS,
-        "ya_group_panels",
-        true
-      );
+      const options = changeOptionState(mockOptions, "ya_group_panels", true);
 
       expect(options[3].state).to.be.true;
     });
 
     it("Should change a state of nested option 'ya_fonts' to true", function () {
-      const options = changeOptionState(DEFAULT_OPTIONS, "ya_fonts", true);
+      const options = changeOptionState(mockOptions, "ya_fonts", true);
 
       expect(options[2].items[0].state).to.be.true;
     });
 
     it("Should change a state of nested option 'ya_fonts' to false", function () {
-      const options = changeOptionState(DEFAULT_OPTIONS, "ya_fonts", false);
+      const options = changeOptionState(mockOptions, "ya_fonts", false);
 
       expect(options[2].items[0].state).to.be.false;
     });
@@ -90,18 +80,16 @@ describe("Background", function () {
       expect(filterOptionsByState).to.not.undefined;
     });
 
-    it("Should exist .DEFAULT_OPTIONS", function () {
-      expect(DEFAULT_OPTIONS).to.not.undefined;
-    });
-
     it("Should filter the list only by state equals true", function () {
-      const list = filterOptionsByState(DEFAULT_OPTIONS, true);
-      expect(list).to.have.lengthOf(2);
-      expect(list).to.have.members(["ya_group_fonts", "ya_group_panels"]);
+      const options = changeOptionState(mockOptions, "ya_fonts", true);
+      const list = filterOptionsByState(options, true);
+
+      expect(list).to.have.lengthOf(1);
+      expect(list).to.have.members(["ya_fonts"]);
     });
 
     it("Should filter the list only by state equals false", function () {
-      const list = filterOptionsByState(DEFAULT_OPTIONS, false);
+      const list = filterOptionsByState(mockOptions, false);
       expect(list).to.have.lengthOf(5);
       expect(list).to.have.members([
         "ya_search",
@@ -110,6 +98,42 @@ describe("Background", function () {
         "ya_font_size",
         "ya_side_panel",
       ]);
+    });
+  });
+
+  describe(".convertOptions()", function () {
+    it("Should exist Function", function () {
+      expect(convertOptions).to.not.undefined;
+    });
+
+    it("Should return the default options when Null is passed", function () {
+      expect(convertOptions(null)).to.be.instanceOf(Array);
+    });
+
+    it("Should return the default options on the empty object ", function () {
+      expect(convertOptions({})).to.be.instanceOf(Array);
+    });
+
+    it("Should return the converted object", function () {
+      mockOptions = {
+        ya_search: false,
+        ya_fonts: false,
+        ya_font_size: false,
+        ya_side_panel: false,
+      };
+      expect(convertOptions(mockOptions)).to.be.instanceOf(Array);
+    });
+
+    it("Should keep values of old object-like options ", function () {
+      mockOptions = {
+        ya_search: false,
+        ya_fonts: true,
+        ya_font_size: false,
+        ya_side_panel: true,
+      };
+      let convertedList = convertOptions(mockOptions);
+
+      expect(filterOptionsByState(convertedList, true)).to.be.lengthOf(2);
     });
   });
 });
